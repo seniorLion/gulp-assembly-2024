3535aaecf57f2e31eaf9583221b6adf92e7e8001
Igori Galanchuk <prieomnaeakomissiea@gmail.com>
2024-09-24 07:23:56 +0300

    new

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,27 @@
-
-
 const gulp = require('gulp')
-const less = require('less')
+const less = require('gulp-less')
 const del = require('del')
+
+const path = {
+  styles: {
+    src: 'src/styles/**/*.less',
+    dest: 'dist/css'
+  },
+  scripts: {
+    src: 'src/scripts/**/*.js',
+    dest: 'dist/js'
+  }
+}
 
 function clean() {
   return del(['dist'])
 }
 
+function styles() {
+  return gulp.src(path.styles.src)
+  .pipe(less())
+  .pipe(gulp.dest(path.styles.dest))
+}
+
 exports.clean = clean
+exports.styles = styles
